0e5ccfa9e4ac9fff2f04a7fd158f5ef42a25e2ca
モヤっとデータサイエンティスト <107286388+moyattodataman@users.noreply.github.com>
2023-03-09 00:56:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,15 +17,15 @@ st.markdown("## さあアップロードしよう👇")
 uploaded_file = st.file_uploader("")
 if uploaded_file is not None:
     # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
+    # bytes_data = uploaded_file.getvalue()
     # st.write(bytes_data)
 
     # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     # st.write(stringio)
 
     # To read file as string:
-    string_data = stringio.read()
+    # string_data = stringio.read()
     # st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
